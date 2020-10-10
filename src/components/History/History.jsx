7e52e4150ad79df21779e7db46c706b669672145
Adorazel <adorazel@gmail.com>
2020-10-10 13:05:04 +0300

    History clean button fix

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -60,15 +60,17 @@ const History = props => {
         }
       </div>
     </div>
-    <div className="history__trash">
-      <Button variant="simple" onClick={cleanHistory}>
-        <svg width="18" height="18" viewBox="0 0 18 18" className="icon" xmlns="http://www.w3.org/2000/svg">
-          <path d="M17.7,16.5c0.4,0.4,0.4,0.9,0,1.3c-0.2,0.2-0.4,0.3-0.6,0.3c-0.3,0-0.5-0.1-0.6-0.3L9,10.3l-7.5,7.5
+    {
+      !!historyItems.length && <div className="history__trash">
+        <Button variant="simple" onClick={cleanHistory}>
+          <svg width="18" height="18" viewBox="0 0 18 18" className="icon" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.7,16.5c0.4,0.4,0.4,0.9,0,1.3c-0.2,0.2-0.4,0.3-0.6,0.3c-0.3,0-0.5-0.1-0.6-0.3L9,10.3l-7.5,7.5
 	c-0.4,0.4-0.9,0.4-1.3,0C0.1,17.5,0,17.4,0,17.1c0-0.3,0.1-0.5,0.3-0.6L7.7,9L0.3,1.5c-0.4-0.4-0.4-0.9,0-1.3s0.9-0.4,1.3,0L9,7.7
 	l7.5-7.5c0.4-0.4,0.9-0.4,1.3,0c0.4,0.4,0.4,0.9,0,1.3L10.3,9L17.7,16.5z"/>
-        </svg>
-      </Button>
-    </div>
+          </svg>
+        </Button>
+      </div>
+    }
   </div>
 }
 
